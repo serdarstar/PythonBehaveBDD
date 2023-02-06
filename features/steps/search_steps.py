5f095a94f4a3bba9d991385dad38f41c4e026f8d
Serdar Yildiz <serdaryildiz.sdet@gmail.com>
2023-02-06 13:04:27 +0000

    Working BDD basics

---
+++ features/steps/search_steps.py
@@ -0,0 +1,35 @@
+import time
+
+from behave import *
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@given(u'I navigate to google.com')
+def step_impl(context):
+
+    context.driver.get("http://google.com")
+    context.driver.implicitly_wait(2)
+    context.driver.find_element_by_xpath("//button[@id='L2AGLb']").click()
+    #input()
+    context.driver.implicitly_wait(2)
+
+
+@when(u'I validate the page title')
+def step_impl(context):
+    title = context.driver.title
+    print("Title is " + title)
+    time.sleep(4)
+    assert "Google" in title
+
+
+
+@then(u'I enter the text as "{searchText}"')
+def step_impl(context,searchText):
+    context.driver.find_element_by_name("q").send_keys(searchText)
+
+
+@then(u'I click the search button')
+def step_impl(context):
+    context.driver.find_element_by_xpath("//input[@title='Search']").click()
+    time.sleep(3)
